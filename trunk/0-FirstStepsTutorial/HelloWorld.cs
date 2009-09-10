19ed56fed9b2720799252a82d3f2dd04985c840b
alexander.maskovyak <alexander.maskovyak@a9ce6336-9ca8-11de-8e8d-3741ea1c8a87>
2009-09-10 01:47:18 +0000

    Updated from examples in class.

--- trunk/0-FirstStepsTutorial/HelloWorld.cs
+++ trunk/0-FirstStepsTutorial/HelloWorld.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace agm
+using COM = System.Console;
+using List = System.Collections.Generic.List<int>;
+// bad: using List = System.Collections.Generic.List;
+using System.Windows;
+
+namespace _0_FirstStepsTutorial
 {
     class HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            COM.WriteLine("Hello, World!");
+            List l = new List();
         }
     }
 }
