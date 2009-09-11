5c8bf3cd47f59420d55e4eed540c3f37156d10de
Alex Maskovyak <alexander.maskovyak@gmail.com>
2009-09-11 20:37:59 +0000

    Altered accessor methods.  Added Equals logic to PlayingCards.  Added uniqueness logic to PokerHand.

--- 1-Poker/Driver.cs
+++ 1-Poker/Driver.cs
@@ -5,18 +5,29 @@ using System.Text;
 
 namespace _1_Poker
 {
+    /// <summary>
+    /// Currently serves as a test for features.  Will eventually be what receives standard-in's series of cards
+    /// to determine whose pokerhand is a winner.
+    /// </summary>
     public class Driver
     {
         static void Main(string[] args)
         {
             PlayingCard c = new PlayingCard( PlayingCard.Value.Ace, PlayingCard.Suit.Club );
             System.Console.WriteLine(c);
-            PokerHand hand = new PokerHand( c, c, c, c, c );
+            PokerHand hand = new PokerHand( c );
 
             string hands = "2H 3D 5S 9C KD 2C 3H 4S 8C AH"; // System.Console.ReadLine();
             while (hands != null)
             {
                 string[] cards = hands.Split(' ');
+                // take half of the cards and add them to one pokerhand
+                // take the other half of the cards and add them to the other pokerhand
+
+                // init the pokerhand ranker, have it create rank objects for both hands
+                // use the overloaded comparison operator on the rank objects to determine the winner
+
+
                 foreach( string card in cards ) 
                 {
                     c = new PlayingCard(card[0], card[1]);
--- 1-Poker/PokerHand.cs
+++ 1-Poker/PokerHand.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,12 @@ namespace _1_Poker
     /// </summary>
     public class PokerHand
     {
-        private PlayingCard[] _playingCards;
+        /// <summary>
+        /// Largest quantity of cards allowed in a 5-card hand of poker.
+        /// </summary>
+        public const int MAX_HAND_SIZE = 5;
+
+        private HashSet<PlayingCard> _playingCards;
 
         /// <summary>
         /// Default cosntructor for creating a hand of playing cards or a 5-card variant of poker.
@@ -18,12 +24,78 @@ namespace _1_Poker
         /// <param name="playingCards">The playing cards to add to this hand.  0 through 5 cards may be passed in.</param>
         public PokerHand( params PlayingCard[] playingCards )
         {
-            if ( playingCards.Length > 5 ) 
+            // check size issues
+            if ( playingCards == null || playingCards.Length > PokerHand.MAX_HAND_SIZE ) 
             {
                 throw new System.ArgumentException( "A PokerHand may only be constructed with 0 to 5 playing cards." );
             }
 
-            _playingCards = playingCards;
+            // create hashset and add cards to it
+            _playingCards = new HashSet<PlayingCard>();
+            foreach (PlayingCard card in playingCards)
+            {
+                _playingCards.Add(card);
+            }
+
+            // the number of cards in the set should match the number of cards passed in
+            // otherwise we have duplicates!
+            if( _playingCards.Count != playingCards.Length )
+            {
+                throw new System.ArgumentException( "A PokerHand may only contain one of any particular card type from a standard 52-card deck!  Cheater!" );
+            }
+        }
+
+        /// <summary>
+        /// Clears the hand of all cards.
+        /// </summary>
+        public void Clear()
+        {
+            _playingCards.Clear();
+        }
+        
+        /// <summary>
+        /// Removes the specified playingcard from this hand.
+        /// </summary>
+        /// <param name="card">PlayingCard to remove.</param>
+        public void Remove(PlayingCard card)
+        {
+            _playingCards.Remove(card);
+        }
+
+        /// <summary>
+        /// Obtains the number of cards in this hand.
+        /// </summary>
+        /// <returns>Count of the cards in this hand.</returns>
+        public int HandSize()
+        {
+            return _playingCards.Count;
+        }
+
+        /// <summary>
+        /// Attempts to add the PlayingCards to this PokerHand.  The cards will only be added if the hand is not currently full or if it is
+        /// a card that this hand does not already contain.
+        /// </summary>
+        /// <param name="cards">PlayingCards to be added.</param>
+        /// <returns>True if all of the cards were added, false otherwise.</returns>
+        public bool Add(params PlayingCard[] cards)
+        {
+            bool result = true;
+            foreach (PlayingCard card in cards)
+            {
+                if (PokerHand.MAX_HAND_SIZE == HandSize)
+                {
+                    return false;
+                }
+
+                if (_playingCards.Contains(card))
+                {
+                    throw new System.ArgumentException("A PokerHand may only contain one of any particular card type from a standard 52-card deck!  Cheater!");
+                }
+
+                result = result && _playingCards.Add(card);
+            }
+
+            return result;
         }
     }
 }
