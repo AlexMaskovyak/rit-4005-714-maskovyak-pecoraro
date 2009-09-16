599498fbb97232debedd59948ef7280edde6ea43
joepeck02 <joepeck02@a9ce6336-9ca8-11de-8e8d-3741ea1c8a87>
2009-09-16 18:49:59 +0000

    Removed manual enum values

--- trunk/1-Poker/PlayingCard.cs
+++ trunk/1-Poker/PlayingCard.cs
@@ -18,7 +18,7 @@ namespace _1_Poker
         /// <summary>
         /// Playing card ranks/values.  The "number" of the card.
         /// </summary>
-        public enum Ranks { NAR=-1, Two=2, Three=3, Four=4, Five=5, Six=6, Seven=7, Eight=8, Nine=9, Ten=10, Jack=11, Queen=12, King=13, Ace=14  }
+        public enum Ranks { NAR=-1, Two=2, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace }
 
         private readonly PlayingCard.Suits _suit;
         private readonly PlayingCard.Ranks _rank;
