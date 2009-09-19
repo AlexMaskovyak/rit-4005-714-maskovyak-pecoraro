4b3f3e36a68937b118ea69c698b26da73b6edbe6
Alex Maskovyak <alexander.maskovyak@gmail.com>
2009-09-19 05:15:41 +0000

    Created default constructor that creates a deck of cards as well as a shuffle method using Knuth's shuffle.

--- 2-PokerPuzzle/Deck.cs
+++ 2-PokerPuzzle/Deck.cs
@@ -15,8 +15,41 @@ namespace _2_PokerPuzzle
     /// </remarks>
     public class Deck {
 
-        public IEnumerable<PlayingCard> Shuffle() {
+        /// <summary>
+        /// The number of PlayingCards in a standard Deck of Cards.
+        /// </summary>
+        public const int DECK_SIZE = 52;
 
+        private readonly PlayingCard[] playingCards;
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public Deck() {
+            playingCards = new PlayingCard[ Deck.DECK_SIZE ];
+
+            int index = 0;
+            // create playingcards
+            foreach( string suit in Enum.GetNames(typeof(PlayingCard.Suits)) ) {
+                foreach( string rank in Enum.GetNames(typeof(PlayingCard.Ranks)) ) {
+                    playingCards[index++] = new PlayingCard(rank[0], suit[0]);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Shuffles the PlayingCard s in this deck using Knuth's algorithm.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator<PlayingCard> Shuffle() {
+            Random rng = new Random(); // default constructor automatically uses system time as a seed
+            for(int i = Deck.DECK_SIZE - 1; i > 0; --i ) {
+                int randomNumber = rng.Next(i);
+                PlayingCard temp = playingCards[i];
+                playingCards[i] = playingCards[randomNumber];
+                playingCards[randomNumber] = temp;
+            }
+            return (IEnumerator<PlayingCard>)playingCards.GetEnumerator();
         }
 
     }
