ac3e605b84c75d14e9e048092c8ead1ccf685663
alexander.maskovyak <alexander.maskovyak@a9ce6336-9ca8-11de-8e8d-3741ea1c8a87>
2009-09-22 22:24:36 +0000

    Fixed puzzle's GetBestHand method (contained a bug). Added additional methods of IsBestHand for quality of life reasons.

--- trunk/2-PokerPuzzle/Puzzle.cs
+++ trunk/2-PokerPuzzle/Puzzle.cs
@@ -10,7 +10,7 @@ namespace _2_PokerPuzzle
 {
     /// <summary>
     /// Holds a series of PlayingCards and is capable of determining the best possible PokerHand that can
-    /// be created from those hands.  SHOULD THIS INHERIT FROM POKERHAND?
+    /// be created from those hands. 
     /// </summary>
     class Puzzle : PokerHand
     {
@@ -29,7 +29,7 @@ namespace _2_PokerPuzzle
         /// <summary>Simpler Accessor for the Best Hand</summary>
         /// <remarks>This is Not Cached. It is recomputed each access.</remarks>
         public PokerHand BestHand {
-            get { return GetBestHandPossibleAlex(); }
+            get { return GetBestHandPossible(); }
         }
 
         /// <summary>Get the Results for some Selections</summary>
@@ -93,47 +93,66 @@ namespace _2_PokerPuzzle
         }
         */
 
-        /// <summary>Finds the best scoring PokerHand possible from the cards in this Puzzle.</summary>
+        /// <summary>
+        /// Finds the best scoring PokerHand possible from the cards in this Puzzle.
+        /// </summary>
         /// <returns>PokerHand containing the best score possible from the cards in this Puzzle.</returns>
-        public virtual PokerHand GetBestHandPossibleAlex() {
-            return GetBestHandPossibleAlex( 
-                        base.Cards, 
-                        0, 
-                        PokerHand.StandardHandSize, 
-                        new PokerHand(base.Cards[0], base.Cards[1], base.Cards[2], base.Cards[3], base.Cards[4]), 
-                        new PlayingCard[0] );
+        public virtual PokerHand GetBestHandPossible() {
+            return GetBestHandPossible(
+                        base.Cards,
+                        0,
+                        PokerHand.StandardHandSize,
+                        // baseline pokerhand is just the first 5 cards
+                        new PokerHand(base.Cards[0], base.Cards[1], base.Cards[2], base.Cards[3], base.Cards[4]),
+                        // the first array of cards is empty
+                        new PlayingCard[0]);
         }
 
-
-        // TODO: Alex, Fill this in!
-        /// <summary>Recursive Algorithm to Find the Best Possible Hand</summary>
-        /// <param name="cards"></param>
-        /// <param name="arrayPosition"></param>
-        /// <param name="cardsToSelect"></param>
-        /// <param name="bestHand"></param>
-        /// <param name="handInProgress"></param>
-        /// <returns></returns>
-        private PokerHand GetBestHandPossibleAlex(
-                PlayingCard[] cards, 
-                int arrayPosition, 
+        /// <summary>Finds the best scoring PokerHand possible from the cards specified.</summary>
+        /// <remarks>
+        /// Uses a recursive algorithm where every run of the function generates the best possible hand
+        /// containing the PlayingCard at the array position and the best possible hand that does not
+        /// contain the PlayingCard at the array position.
+        /// </remarks>
+        /// <param name="cards">Array of cards from which to find the best scoring hand.</param>
+        /// <param name="arrayPosition">Position indexing into the card's array, specifying the card to include and exclude.</param>
+        /// <param name="cardsToSelect">Maximum number of cards out of which to create a PokerHand for scoring.</param>
+        /// <param name="bestHand">The best scoring PokerHand found so far.</param>
+        /// <param name="handInProgress">Array of cards being built up over recursive calls to this function.</param>
+        /// <returns>
+        /// The best scoring PokerHand formed from the cards passed into this arry starting
+        /// at the index specified.
+        /// </returns>
+        private PokerHand GetBestHandPossible(
+                PlayingCard[] cards,
+                int arrayPosition,
                 int cardsToSelect,
                 PokerHand bestHand,
                 PlayingCard[] handInProgress
-            ) {
+            )
+        {
 
             // we have the necessary quantity of cards to select for a PokerHand
-            if( handInProgress.Length == cardsToSelect ) {
-                return GetBestHand( bestHand, new PokerHand( handInProgress ) );
+            // so create and compare the PokerHands
+            if (handInProgress.Length == cardsToSelect) {
+                return GetBestHand(bestHand, new PokerHand(handInProgress));
             }
-            
+
             // we have too few cards and have run out of cards to select
-            if( arrayPosition == cards.Length ) {
+            if (arrayPosition == cards.Length) {
+                return bestHand;
+            }
+
+            // catch whether we could not possibly get enough cards to form a hand from this
+            // position of the array and on, we'd rather not waste the cycles on calls that
+            // will not produce anything but bestHand as a result
+            if (handInProgress.Length + (cards.Length - arrayPosition) < cardsToSelect) {
                 return bestHand;
             }
 
             // choose to not select this card and move on
-            PokerHand resultNoAdd = 
-                GetBestHandPossibleAlex(
+            PokerHand resultNoAdd =
+                GetBestHandPossible(
                     cards,
                     arrayPosition + 1,
                     cardsToSelect,
@@ -141,27 +160,27 @@ namespace _2_PokerPuzzle
                     handInProgress);
 
             // choose to select this card and move on
-            PlayingCard[] bestHandOfCards2 = new PlayingCard[handInProgress.Length + 1];
-            handInProgress.CopyTo(bestHandOfCards2, 0);
-            bestHandOfCards2[handInProgress.Length] = cards[arrayPosition];
+            PlayingCard[] handInProgress2 = new PlayingCard[handInProgress.Length + 1];
+            handInProgress.CopyTo(handInProgress2, 0);
+            handInProgress2[handInProgress.Length] = cards[arrayPosition];
 
             PokerHand resultAdd =
-                GetBestHandPossibleAlex(
+                GetBestHandPossible(
                     cards,
                     arrayPosition + 1,
                     cardsToSelect,
                     bestHand,
-                    bestHandOfCards2);
+                    handInProgress2);
 
             // compare the result and return the best one
             if (resultNoAdd.HandSize() == PokerHand.StandardHandSize &&
-                resultAdd.HandSize() == PokerHand.StandardHandSize)
-            {
+                resultAdd.HandSize() == PokerHand.StandardHandSize) {
                 return GetBestHand(resultNoAdd, resultAdd);
             }
 
             return bestHand;
         }
+
         
         /// <summary>Compares Two Hands and Returns the better</summary>
         /// <param name="hand1">First Hand</param>
