7160700acecca77092cc808b64d86ba7171a2553
alexander.maskovyak <alexander.maskovyak@a9ce6336-9ca8-11de-8e8d-3741ea1c8a87>
2009-09-26 14:40:36 +0000

    Just the preliminaries.

---
+++ trunk/3-SudokuModel/Test.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _3_SudokuModel
+{
+    /// <summary>Driver class for testing the capabilities of Board.</summary>
+    public class Test
+    {
+        public static void Main(string[] args)
+        {
+            // determine whether to display output
+            bool debug = (args.Length > 0);
+
+            // create the string array of rows to build our Board
+            List<string> rows = new List<string>();
+            string input;
+            while ((input = Console.ReadLine().Trim()) != null && !input.Equals("")) {
+                rows.Add(input.Trim());
+            }
+
+            // build the board
+            Board board = new Board(rows.ToArray<string>());
+
+            // await set # # commands
+            string[] setCommandArgs;
+            int commandIndex = 0;
+            int cellIndex = 1;
+            int valueIndex = 2;
+            while ((input = Console.ReadLine().Trim()) != null && !input.Equals("")) {
+                
+                setCommandArgs = input.Split(' ', ':');
+                if (!setCommandArgs[commandIndex].Equals("set") || setCommandArgs.Length != 3) {
+                    break;
+                }
+                board.Set(
+                    int.Parse(setCommandArgs[cellIndex]), 
+                    int.Parse(setCommandArgs[valueIndex]));
+            }
+
+            Console.WriteLine("tada");
+        }
+    }
+}
