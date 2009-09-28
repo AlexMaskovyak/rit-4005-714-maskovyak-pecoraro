031f8f542d2a84b543d788392ebc1ed717ac076a
Alex Maskovyak <alexander.maskovyak@gmail.com>
2009-09-28 00:15:31 +0000

    Successfully got the delegate stuff to work.  It needs to be changed to actually implement the observer pattern I feel...

---
+++ 3-SudokuModel/AssignableCell.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _3_SudokuModel
+{
+    /// <summary></summary>
+    public class AssignableCell : Cell
+    {
+        private Boolean _validValue;
+
+
+        public override int Value {
+            get { return base.Value; }
+            set { _value = value; base.observers(this); }
+        }
+
+        public AssignableCell() : base(-1) {
+        }
+
+        public static void Main() {
+            AssignableCell cell = new AssignableCell();
+            CellRegion column = new CellRegion("column");
+            CellRegion row = new CellRegion("row");
+            CellRegion shape = new CellRegion("shape");
+            column.Observe(cell);
+            row.Observe(cell);
+            shape.Observe(cell);
+            cell.Value = 2;
+        }
+    }
+}
---
+++ 3-SudokuModel/CellRegion.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _3_SudokuModel
+{
+    public class CellRegion
+    {
+        private string _name;
+        private List<Cell> _cells;
+        private HashSet<int> _valuesHeld;
+
+        public CellRegion(string name) {
+            _name = name;
+            _cells = new List<Cell>();
+            _valuesHeld = new HashSet<int>();
+        }
+
+
+
+        public void Observe(Cell cell) {
+            cell.observers += new Cell.ValueAssigned(this.ValueCheck);
+            _cells.Add(cell);
+        }
+
+
+
+        /// <summary>
+        /// Determines whether 
+        /// </summary>
+        /// <param name="cell"></param>
+        public void ValueCheck(Cell cell) {
+            
+            int i = cell.Value;
+
+            Console.WriteLine(String.Format("{0} has found cell has: {1}", _name, cell.Value));
+        }
+
+    }
+}
