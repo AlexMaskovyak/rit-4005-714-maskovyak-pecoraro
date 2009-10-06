1eba5002a6fab370566a9174fe771dac7fb5edf3
alexander.maskovyak <alexander.maskovyak@a9ce6336-9ca8-11de-8e8d-3741ea1c8a87>
2009-10-06 03:00:10 +0000

    Added a tester to view the controls.  Updated SudokuCelluserControl to programmtically create the grid.

--- trunk/4-SudokuView-UserControlLibrary/SudokuCellUserControl.xaml.cs
+++ trunk/4-SudokuView-UserControlLibrary/SudokuCellUserControl.xaml.cs
@@ -23,13 +23,20 @@ namespace _4_SudokuView_UserControlLibrary
     {
         protected Label[] _digitLabels;
         protected SudokuViewWindow _window;
-        //protected ObservableBoard _board;
 
-        public SudokuCellUserControl(SudokuViewWindow window)
+        public SudokuCellUserControl()
         {
+            CreateGrid(3, 3);
+            InitializeComponent();
+        }
+
+        public SudokuCellUserControl(SudokuViewWindow window, SolidColorBrush brush)
+        {
+            
+
             InitializeComponent();
             _window = window;
-            _digitLabels = new Label[] { 
+            /*_digitLabels = new Label[] { 
                 this.BigDigitLabel, 
                 this.LittleDigit1Label, 
                 this.LittleDigit2Label, 
@@ -38,15 +45,53 @@ namespace _4_SudokuView_UserControlLibrary
                 this.LittleDigit5Label,
                 this.LittleDigit7Label, 
                 this.LittleDigit8Label, 
-                this.LittleDigit9Label };
+                this.LittleDigit9Label };*/
             //_board = board;
+        }
+
+        protected virtual void CreateGrid(int colDimension, int rowDimension)
+        {
+            Grid myGrid = new Grid();
+            myGrid.Height = 100;
+            myGrid.Width = 100;
+            myGrid.HorizontalAlignment = HorizontalAlignment.Center;
+            myGrid.VerticalAlignment = VerticalAlignment.Center;
+
+            this.Content = myGrid;
+
+            for (var col = 0; col < colDimension; ++col) {
+                myGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            for (var row = 0; row < rowDimension; ++row) {
+                myGrid.RowDefinitions.Add(new RowDefinition());
+            }
+
+            for (var row = 0; row < rowDimension; ++row) {
+                for (var col = 0; col < colDimension; ++col) {
+                    TextBlock littleDigitBlock = new TextBlock();
+                    littleDigitBlock.Text = ((colDimension * row) + col + 1).ToString();
+                    littleDigitBlock.FontSize = 12;
+                    Grid.SetRow(littleDigitBlock, row);
+                    Grid.SetColumn(littleDigitBlock, col);
+                    myGrid.Children.Add(littleDigitBlock);
+                }
+            }
+
+            TextBlock bigDigitBlock = new TextBlock();
+            bigDigitBlock.Text = "0";
+            Grid.SetRow(bigDigitBlock, 0);
+            Grid.SetColumn(bigDigitBlock, 0);
+            Grid.SetRowSpan(bigDigitBlock, rowDimension);
+            Grid.SetColumnSpan(bigDigitBlock, colDimension);
+            myGrid.Children.Add(bigDigitBlock);
         }
 
         /// <summary>
         /// Sets the value of the central large digit.
         /// </summary>
         /// <param name="digit">Value assigned to this cell.</param>
-        protected virtual void SetBigDigit(int digit)
+        /*protected virtual void SetBigDigit(int digit)
         {
             this.BigDigitLabel.Content = digit;
             this.BigDigitLabel.Visibility = System.Windows.Visibility.Visible;
@@ -67,19 +112,20 @@ namespace _4_SudokuView_UserControlLibrary
             {
                 _digitLabels[digit].Visibility = System.Windows.Visibility.Visible;
             }
-        }
+        }*/
 
 
-        public void Click(System.Object sender, EventArgs e)
+        public virtual void Click(System.Object sender, EventArgs e)
         {
             // this should communicate with the board to set the cell to the value
             //_board.((Label)sender).Content
             
         }
 
-        public void Update()
+        public virtual void Update()
         {
 
         }
+
     }
 }
