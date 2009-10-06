70bbc007f5025ca6e1db88921bdd95e556766716
Alex Maskovyak <alexander.maskovyak@gmail.com>
2009-10-06 00:54:54 +0000

    SudokuCommand correctly takes a board reference on do and undo.

--- 4-SudokuView/SudokuCommand.cs
+++ 4-SudokuView/SudokuCommand.cs
@@ -14,44 +14,44 @@ namespace _4_SudokuView
         public enum Type { SET, CLEAR };
 
         protected SudokuCommand.Type _type;
-        protected IBoardAdvanced _board;
         protected int _cellIndex;
-        protected int? _digit;
+        protected int _digit;
 
         /// <summary>Default constructor.</summary>
         /// <param name="type">Type of Sudoku command.</param>
         /// <param name="board">Reference to board affected.</param>
         /// <param name="cellIndex">Index this command operated upon.</param>
         /// <param name="digit">Previous digit.</param>
-        public SudokuCommand(SudokuCommand.Type type, IBoardAdvanced board, int cellIndex, int? digit) {
+        public SudokuCommand(SudokuCommand.Type type, int cellIndex, int digit) {
             _type = type;
-            _board = board;
             _cellIndex = cellIndex;
             _digit = digit;
         }
 
         /// <summary>Does the Command operation on the Board.</summary>
-        public void Do() {
+        /// <param name="board">Board upon which to process this command.</param>
+        public void Do(IBoardAdvanced board) {
             switch (_type)
             {
                 case Type.CLEAR:
-                    _board.Clear(_cellIndex);
+                    board.Clear(_cellIndex);
                     break;
                 case Type.SET:
-                    _board.Set(_cellIndex, _digit);
+                    board.Set(_cellIndex, _digit);
                     break;
             }
         }
 
         /// <summary>Undoes the Command operation on the Board.</summary>
-        public void Undo() {
+        /// <param name="board">Board upon which to process thic command.</param>
+        public void Undo(IBoardAdvanced board) {
             switch (_type)
             {
                 case Type.CLEAR:
-                    _board.Set(_cellIndex, _digit);
+                    board.Set(_cellIndex, _digit);
                     break;
                 case Type.SET:
-                    _board.Clear(_cellIndex);
+                    board.Clear(_cellIndex);
                     break;
             }
         }
