60624172b9d452324391b7617ab38368f29f0221
alexander.maskovyak <alexander.maskovyak@a9ce6336-9ca8-11de-8e8d-3741ea1c8a87>
2009-10-08 21:30:01 +0000

    Compiles correctly now.  SuitPrecedencePlayingCard still needs to be updated to function correctly.

--- trunk/5-SelectingAWinner-ConsoleApplication/Player.cs
+++ trunk/5-SelectingAWinner-ConsoleApplication/Player.cs
@@ -18,15 +18,23 @@ namespace _5_SelectingAWinner_ConsoleApplication
 // IView implementation
 
         /// <summary> return <c>0..m-1</c>, index of chosen (and unexposed) card. </summary>
-        public int Choose();
+        public int Choose() {
+            throw new NotImplementedException();
+        }
 
         /// <summary> find out about a chosen card. </summary>
-        public void Tell(int index, int suit, int value);
+        public void Tell(int index, int suit, int value) {
+            throw new NotImplementedException();
+        }
 
         /// <summary> find out about a round's outcome. </summary>
-        public void Winner(bool yes);
+        public void Winner(bool yes) {
+            throw new NotImplementedException();
+        }
 
         /// <summary> return once view is ready for a new round. </summary>
-        public void Ready();
+        public void Ready() {
+            throw new NotImplementedException();
+        }
     }
 }
