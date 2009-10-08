708973ae585b19b2941e5fbcef94af8ea2c54f5f
Alex Maskovyak <alexander.maskovyak@gmail.com>
2009-10-08 02:25:56 +0000

    Added projects for assignment 5.

--- 3-SudokuTesterAdvanced/TestAdvanced.cs
+++ 3-SudokuTesterAdvanced/TestAdvanced.cs
@@ -9,13 +9,24 @@ using _3_SudokuTester;
 
 namespace _3_SudokuTesterAdvanced {
 
+    /// <summary>Board which overrides hook methods for easy display of values.</summary>
     public class MyBoard : BoardAdvanced {
+
+        /// <summary>Default constructor.</summary>
+        /// <param name="args">Rows for a board.</param>
         public MyBoard(string[] args) : base(args) { }
 
+        /// <summary>Does nothing.  Simply for testing.</summary>
+        /// <param name="cell">Cell index that has been set.</param>
+        /// <param name="digit">Value it has been set.</param>
         public override void NowSet(int cell, int digit) {
             // Testing New Version.
             //Console.WriteLine("{0} now set with {1}", cell, digit);
         }
+
+        /// <summary>Displays the digits that are now possible for the given cell index.</summary>
+        /// <param name="cell">Cell index.</param>
+        /// <param name="digit">Digits that are now possible.</param>
         public override void NowPossible(int cell, System.Collections.BitArray digit) {
             StringBuilder str = new StringBuilder();
             for (int i = 0; i < digit.Length; ++i) {
---
+++ 5-SelectingAWinner-ConsoleApplication/Referee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _5_SelectingAWinner_ConsoleApplication
+{
+    /// <summary>Runs one or more rounds of the Random selection game.</summary>
+    public class Referee
+    {
+
+        /// <summary>Test for Referee</summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // end
+            if (args == null || args.Count != 3) {
+                
+            }
+
+            int numberOfPlayers = int.Parse(args[0]);
+            int numberOfCards = int.Parse(args[1]);
+            string urlOfCardsImages = args[2];
+
+            // create referee
+            Referee referee = new Referee();
+
+            // create n views
+
+
+        }
+    }
+}
