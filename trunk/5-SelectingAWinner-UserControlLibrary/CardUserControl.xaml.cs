e66f49bcc37feae4bfb4cc5c05ed3116f824aeab
alexander.maskovyak <alexander.maskovyak@a9ce6336-9ca8-11de-8e8d-3741ea1c8a87>
2009-10-13 01:38:26 +0000

    Re-synching solution.  Placeholder Projects created.

---
+++ trunk/5-SelectingAWinner-UserControlLibrary/CardUserControl.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _5_SelectingAWinner_UserControlLibrary
+{
+    /// <summary>Controls for user interaction with a flippable card.</summary>
+    public partial class CardUserControl : UserControl
+    {
+        public CardUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
