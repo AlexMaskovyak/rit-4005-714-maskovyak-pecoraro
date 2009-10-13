b2e74c0b05f25feccf69483602c38924f4ebed28
alexander.maskovyak <alexander.maskovyak@a9ce6336-9ca8-11de-8e8d-3741ea1c8a87>
2009-10-13 02:56:12 +0000

    Re-sync.

---
+++ trunk/5-SelectingAWinner-UserControlLibrary/CardUserControl.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _5_SelectingAWinner_UserControlLibrary
+{
+    /// <summary>Controls for user interaction with a flippable card.</summary>
+    public partial class CardUserControl : UserControl
+    {
+
+        protected Image _back;
+        protected Image _front;
+
+        /// <summary> image to display on the front of the card. </summary>
+        public Image Front {
+            get { return _front;  }
+            set { _front = value;  }
+        }
+
+        /// <summary> image to display on the back of the card. </summary>
+        public Image Back {
+            get { return _back; }
+            set { _back = value; }
+        }
+
+
+        public CardUserControl()
+        {
+            InitializeComponent();
+        }
+
+// Properties
+
+    }
+}
