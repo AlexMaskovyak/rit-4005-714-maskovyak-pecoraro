1f9429ef5868b12b39ad1c014d9986a270c2cea9
alexander.maskovyak <alexander.maskovyak@a9ce6336-9ca8-11de-8e8d-3741ea1c8a87>
2009-10-20 19:14:29 +0000

    Renamed proxyplayer.

---
+++ trunk/6-DistributedWinner-Client/Remote.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+namespace _6_DistributedWinner_Client
+{
+    /// <summary></summary>
+    public class Remote
+    {
+        public Remote()
+        {
+
+        }
+
+        /// <summary> return <c>0..m-1</c>, index of chosen (and unexposed) card. </summary>
+        public int Choose()
+        {
+            return 0;
+        }
+
+        /// <summary> find out about a chosen card. </summary>
+        public void Tell(int index, int suit, int value)
+        {
+
+        }
+
+        /// <summary> find out about a round's outcome. </summary>
+        public void Winner(bool yes)
+        {
+
+        }
+
+        /// <summary> return once view is ready for a new round. </summary>
+        public void Ready()
+        {
+
+        }
+    }
+}
