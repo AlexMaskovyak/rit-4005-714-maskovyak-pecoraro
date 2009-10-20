15a2821e75119d7b7e48de55ce09b33a2b5d698a
alexander.maskovyak <alexander.maskovyak@a9ce6336-9ca8-11de-8e8d-3741ea1c8a87>
2009-10-20 11:54:51 +0000

    Added Professor's Project for assignment 5, created service project, client project for assignment 6.

---
+++ trunk/6-DistributedWinner-Client/ProxyPlayer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using ATS.Winner;
+
+namespace _6_DistributedWinner_Client
+{
+    /// <summary></summary>
+    public class ProxyPlayer : IView
+    {
+        public ProxyPlayer()
+        {
+
+        }
+
+        /// <summary> return <c>0..m-1</c>, index of chosen (and unexposed) card. </summary>
+        public int Choose()
+        {
+
+        }
+
+        /// <summary> find out about a chosen card. </summary>
+        public void Tell(int index, int suit, int value)
+        {
+
+        }
+
+        /// <summary> find out about a round's outcome. </summary>
+        public void Winner(bool yes)
+        {
+
+        }
+
+        /// <summary> return once view is ready for a new round. </summary>
+        public void Ready()
+        {
+
+        }
+    }
+}
---
+++ trunk/6-DistributedWinner/Service1.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="Service1.asmx.cs" Class="_6_DistributedWinner.Service1" %>
