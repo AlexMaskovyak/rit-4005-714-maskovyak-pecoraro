ab11928a1a265e2cb63a2dffd9db511b9a4aeddb
Alex Maskovyak <alexander.maskovyak@gmail.com>
2009-10-25 17:58:39 +0000

    Initial commit.

---
+++ 7-Database/LocalDB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _7_Database
+{
+    /// <summary> facade for an IModel database. </summary>
+    public class LocalDB
+    {
+
+        //public static void Main(string[] args) {
+
+        //}
+    }
+}
