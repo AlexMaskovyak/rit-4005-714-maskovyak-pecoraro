d88940c88edba87711b0b3d408cb6d75dfa1a215
Joseph Pecoraro <joepeck02@gmail.com>
2009-10-26 05:44:03 +0000

    - Updated IDB and DB definitions - Updated Documentation describing actions - Implemented DBTest and Tests - Included Test input file

--- 7-Database/DB.cs
+++ 7-Database/DB.cs
@@ -9,63 +9,113 @@ namespace _7_Database
     /// <typeparam name="T"> type of a word in a tuple. </typeparam>
     public class DB<T> : IDB<T> {
 
+// Fields
+
         /// <summary> holds tuples. </summary>
         protected ICollection<T[]> _tuples;
+
+// Constructor
         
         /// <summary> default constructor. </summary>
         public DB() {
-            _tuples = new List<T[]>();
+            _tuples = CreateTupleCollection();
         }
 
+// Factory methods
+
+        protected virtual ICollection<T[]> CreateTupleCollection() {
+            return new List<T[]>();
+        }
+
+// IDB Interface
+
         /// <summary> adds a tuple. </summary>
+        /// <remarks>
+        ///   the "match" predicate function may match multiple rows. this
+        ///   performs a <c>Delete(match)</c> first, and then adds the new tuple.
+        /// </remarks>
         /// <param name="match"> delegate which specifies how a tuple can match. </param>
         /// <param name="tuple"> tuple to add. </param>
         /// <returns> 
-        /// true if it adds and replaces a tuple with equal content 
-        /// as determined by the match argument, false otherwise. 
+        ///   true if there was a tuple determined to be equal by the "match" predicate
+        ///   which the new tuple replaced. false if no tuples were replaced.
         /// </returns>
         public virtual bool Add(Predicate<T[]> match, T[] tuple) {
-            foreach (T[] t in _tuples) {
-                if (match.Invoke(t)) {
-                    return false;
-                }
-            }
-
-            _tuples.Add(tuple);
-            
-            return true;
+            int removed = Delete(match); // may delete multiples
+            _tuples.Add(tuple);          // always add
+            return (removed > 0);        // indicate if we replaced something
         }
 
         /// <summary> 
-        /// extracts an array of values where each value is constructed 
-        /// by the report argument from one tuple selected by the 
-        /// match argument. 
+        ///   produces an array of values by taking all the tuples that
+        ///   match the provided "match" predicate and running them through
+        ///   the "report" function to tranform the tuple to a desired representation.
         /// </summary>
         /// <param name="match"> delegate specifying if a tuple matches. </param>
-        /// <param name="result"> delegate describing how to construct an extracted tuple </param>
-        /// <returns> an aray, possibly empty, of value. </returns>
-        public virtual T[] Extract(Predicate<T[]> match, Func<T[], T> result) {
-            ICollection<T> values = new List<T>();
+        /// <param name="result"> delegate describing how to represent an extracted tuple. </param>
+        /// <returns> an array, possibly empty, of transformed tuples. </returns>
+        public virtual R[] Extract<R>(Predicate<T[]> match, Func<T[], R> result) {
+            ICollection<R> values = new List<R>();
             foreach( T[] tuple in _tuples ) {
                 if (match.Invoke(tuple)) {
                     values.Add(result.Invoke(tuple));
                 }
             }
-            return values.ToArray<T>();
+            return values.ToArray<R>();
         }
 
         /// <summary> removes all tuples selected by the match argument. </summary>
         /// <param name="match"> delegate specifying if a tuple matches. </param>
         /// <returns> number of tuples that were removed. </returns>
         public virtual int Delete( Predicate<T[]> match) {
-            int removed = 0;
+            List<T[]> toRemove = new List<T[]>();
             foreach( T[] tuple in _tuples ) {
                 if (match.Invoke(tuple)) {
-                    _tuples.Remove(tuple);
-                    ++removed;
+                    toRemove.Add(tuple);
                 }
             }
-            return removed;
+
+            foreach (T[] tuple in toRemove) {
+                _tuples.Remove(tuple);
+            }
+
+            return toRemove.Count;
         }
+
+// Debug
+
+        /// <summary> useful for debugging </summary>
+        /// <returns> print out the database </returns>
+        public override string ToString() {
+            return DB<T>.TuplesAsString(_tuples);
+        }
+
+        /// <summary> useful for debugging </summary>
+        /// <param name="tuples"> prettyprint a list of tuples </param>
+        /// <returns></returns>
+        public static string TuplesAsString(ICollection<T[]> tuples) {
+            StringBuilder str = new StringBuilder();
+            str.Append("[ ");
+            bool first = true;
+            foreach (T[] tuple in tuples) {
+                if (!first) str.Append(",\n  ");
+                str.Append(DB<T>.TupleAsString(tuple));
+                first = false;
+            }
+            str.Append(" ]");
+            return str.ToString();
+        }
+
+        /// <summary> useful for debugging </summary>
+        /// <param name="tuple"> pretty-print a tuple </param>
+        /// <returns> a string representation of a tuple </returns>
+        public static string TupleAsString(T[] tuple) {
+            StringBuilder str = new StringBuilder();
+            str.Append("{");
+            str.Append(String.Join(",", Array.ConvertAll<T, string>(tuple, delegate(T e) { return e.ToString(); })));
+            str.Append("}");
+            return str.ToString();
+        }
+
     }
 }
--- 7-Database/DBTest.cs
+++ 7-Database/DBTest.cs
@@ -2,25 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+
 
 namespace _7_Database
 {
     /// <summary> test of DB functionality. </summary>
     public class DBTest {
 
+// Fields
+
+        /// <summary> the database </summary>
         protected DB<string> _db;
+
+// Constructors
 
         /// <summary> default constructor. </summary>
         public DBTest() {
-
             _db = new DB<string>();
+        }
 
-            // read commands
+// Public Methods
+
+        /// <summary> driver </summary>
+        public virtual void Run() {
             List<string> commands = ReadUntilBlankOrEnd();
-
-            // execute commands
             RunCommands(commands);
         }
+
+// Protected Methods
 
         /// <summary>Reads lines from Standard Input until the first blank line</summary>
         /// <returns>The list of lines</returns>
@@ -31,76 +41,162 @@ namespace _7_Database
                 input = input.Trim();
                 if (input.Equals("")) {
                     break;
-                }
-                else {
+                } else {
                     rows.Add(input);
                 }
             }
             return rows;
         }
 
-        /// <summary>Process commands.</summary>
+        /// <summary>Read all commands and process each individually.</summary>
         /// <param name="commands">The command lines.</param>
         protected virtual void RunCommands(List<string> commands) {
             foreach (string line in commands) {
 
-                // Split the Arguments to determine the action
+                // Split the Arguments to determine the action and args
                 string[] arguments = line.Split('\t');
+                string action = arguments[0];
+                string[] actionArguments = arguments.Skip(1).ToArray<string>();
                 
                 // try to process this through the commands that are known
-                ProcessAddCommand(arguments);
+                Console.WriteLine("> " + line);
+                ProcessCommand(action, actionArguments);
+                Console.WriteLine();
+
             }
         }
 
-        /// <summary> processes an add command, adding a tuple of 0 or more words to the database. </summary>
-        /// <param name="arguments"> 
-        /// has the form: "add", word*
-        /// </param>
-        protected virtual void ProcessAddCommand(string[] arguments) {
-            // check to ensure we know what to do with this
-            if( !arguments[0].Equals("add") ) {
-                return;
+        /// <summary> process a single command, template method </summary>
+        /// <param name="action"> the command's action </param>
+        /// <param name="args"> the command's arguments </param>
+        protected virtual void ProcessCommand(string action, string[] args) {
+            switch (action) {
+                case "add":
+                    ProcessAddCommand(args);
+                    break;
+                case "extract":
+                    ProcessExtractCommand(args);
+                    break;
+                case "delete":
+                    ProcessDeleteCommand(args);
+                    break;
+                case "debug":
+                    ProcessDebugCommand(args);
+                    break;
+                default:
+                    Console.Error.WriteLine("Unknown Command: {0}", action);
+                    break;
             }
+        }
 
+        /// <summary> summary of the db </summary>
+        /// <param name="args"> ignored arguments </param>
+        protected virtual void ProcessDebugCommand(string[] args) {
+            Console.WriteLine(_db.ToString());
+        }
 
-            _db.Add(
-                new Predicate<string[]>(
-                    delegate(string[] dbtuple, string tupleToAdd) {
-                        foreach (string word in tuple) {
+        /// <summary> processes an add command, adding a tuple of 0 or more words to the database. </summary>
+        /// <param name="arguments"> arguments after "add" </param>
+        protected virtual void ProcessAddCommand(string[] newRecord) {
+            bool added = _db.Add(CreateSimpleMatcher(newRecord), newRecord);
+        }
 
-                        }
-                        return true; 
-                    } 
-                ),
-                arguments.Skip(1).ToArray<string>());
+        /// <summary> processes a delete command, removing a tuple matching the recordTemplate </summary>
+        /// <param name="recordTemplate"> arguments after "delete" </param>
+        protected virtual void ProcessDeleteCommand(string[] args) {
+            int field = int.Parse(args[0]);
+            string regexString = args[1];
+            int deleteCount = _db.Delete(CreateSingleFieldRegexMatcher(field, regexString));
         }
 
 
         /// <summary> 
-        /// processes an extract command on the database, dispalying a value field from all 
-        /// tuples with a key matching a regular expression; fields are numbered from 0. 
+        ///   processes an extract command on the database, dispalying a value field from all 
+        ///   tuples with a key matching a regular expression; fields are numbered from 0. 
         /// </summary>
-        /// <param name="arguments">  
-        /// has the form: "extract" key-position pattern value-position
+        /// <param name="arguments">
+        ///   arguments after "extract"
+        ///   has the form: key-position pattern value-position
         /// </param>
-        protected virtual void ProcessExtractCommand(string[] arguments) {
-            // check to ensure we know what to do
-            if( !arguments[0].Equals("extract") ) {
-                return;
-            }
+        protected virtual void ProcessExtractCommand(string[] args) {
+            int field = int.Parse(args[0]);
+            string regexString = args[1];
+            int desiredFieldIndex = int.Parse(args[2]);
+            string[] extracted = _db.Extract(CreateSingleFieldRegexMatcher(field, regexString), delegate(string[] record) {
+                return (desiredFieldIndex < record.Length) ? record[desiredFieldIndex] : null;
+            });
 
-            _db.Extract(
-                new Predicate<string[]>( delegate(string[] tuple) { return true; } ),
-                new Func<string[], string>( 
-                    delegate(string[] tuple) { return ""; } ) );
-            //new delegate Func<> {}
+            // Output
+            Console.WriteLine(String.Join(",", extracted));
         }
+
+// Delegate Factory Methods
+
+        /// <summary> factory method to create a predicate matcher for a string[] record </summary>
+        /// <param name="newRecord"> the record to be matching against </param>
+        /// <returns> a predicate returning true when a given record matches newRecord </returns>
+        protected virtual Predicate<string[]> CreateSimpleMatcher(string[] recordTemplate) {
+            int length = recordTemplate.Length;
+            return delegate(string[] record) {
+                if (length != record.Length)
+                    return false;
+
+                for (int i = 0; i < length; ++i) {
+                    if (record[i] != recordTemplate[i])   // NOTE: != is okay for strings
+                        return false;
+                }
+
+                return true;
+            };
+        }
+
+        /// <summary> factory method to create a predicate matcher for a single field </summary>
+        /// <param name="field"> the field index </param>
+        /// <param name="str"> the pattern to match against the field value </param>
+        /// <returns> true if matched, false otherwise </returns>
+        protected virtual Predicate<string[]> CreateSingleFieldRegexMatcher(int field, string str) {
+            Regex regex = new Regex(str);
+            return delegate(string[] record) {
+                return (field < record.Length && regex.IsMatch(record[field]));
+            };
+        }
+
+// Not Currently Used...
+
+        /// <summary> factory method to create a predicate matcher for a string[] recordTemplate containing regexes </summary>
+        /// <param name="newRecord"> the record to be matching against </param>
+        /// <returns> a predicate returning true when a given record matches newRecord </returns>
+        protected virtual Predicate<string[]> CreateRegexMatcher(string[] recordTemplate) {
+
+            // Close over regexes and length once and use them in the returned closure
+            int length = recordTemplate.Length;
+            Regex[] regexes = Array.ConvertAll<string, Regex>(recordTemplate, delegate(string str) {
+                if (str == null || str.Length == 0)
+                    return new Regex(".*");   // wildcard match anything
+                return new Regex(str);        // input was a regex
+            });
+
+            // Predicate that runs regexes against each string in the records
+            return delegate(string[] record) {
+                if (length != record.Length)
+                    return false;
+
+                for (int i = 0; i < length; ++i) {
+                    if (!regexes[i].IsMatch(record[i]))
+                        return false;
+                }
+
+                return true;
+            };
+
+        }
+
+// Driver
 
         /// <summary> test the db. </summary>
         /// <param name="args"></param>
         public static void Main(string[] args) {
-
-
+            new DBTest().Run();
         }
     }
 }
