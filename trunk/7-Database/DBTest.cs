fc150da75ea74ac78ff8b48d4a5a8fbfba519c06
alexander.maskovyak <alexander.maskovyak@a9ce6336-9ca8-11de-8e8d-3741ea1c8a87>
2009-10-26 19:57:56 +0000

    My updates, created DBDelegateFactory...updated LocalDB...working on LocalDBTest.

--- trunk/7-Database/DBTest.cs
+++ trunk/7-Database/DBTest.cs
@@ -98,7 +98,7 @@ namespace _7_Database
         /// <summary> processes an add command, adding a tuple of 0 or more words to the database. </summary>
         /// <param name="arguments"> arguments after "add" </param>
         protected virtual void ProcessAddCommand(string[] newRecord) {
-            bool added = _db.Add(CreateSimpleMatcher(newRecord), newRecord);
+            bool added = _db.Add(DBDelegateFactory.CreateSimpleMatcher(newRecord), newRecord);
         }
 
         /// <summary> processes a delete command, removing a tuple matching the recordTemplate </summary>
@@ -106,7 +106,7 @@ namespace _7_Database
         protected virtual void ProcessDeleteCommand(string[] args) {
             int field = int.Parse(args[0]);
             string regexString = args[1];
-            int deleteCount = _db.Delete(CreateSingleFieldRegexMatcher(field, regexString));
+            int deleteCount = _db.Delete(DBDelegateFactory.CreateSingleFieldRegexMatcher(field, regexString));
         }
 
 
@@ -122,74 +122,14 @@ namespace _7_Database
             int field = int.Parse(args[0]);
             string regexString = args[1];
             int desiredFieldIndex = int.Parse(args[2]);
-            string[] extracted = _db.Extract(CreateSingleFieldRegexMatcher(field, regexString), delegate(string[] record) {
-                return (desiredFieldIndex < record.Length) ? record[desiredFieldIndex] : null;
-            });
+            string[] extracted = _db.Extract(
+                DBDelegateFactory.CreateSingleFieldRegexMatcher(field, regexString), 
+                DBDelegateFactory.CreateIndexExtractingReporter(desiredFieldIndex));
 
             // Output
             Console.WriteLine(String.Join(",", extracted));
         }
 
-// Delegate Factory Methods
-
-        /// <summary> factory method to create a predicate matcher for a string[] record </summary>
-        /// <param name="newRecord"> the record to be matching against </param>
-        /// <returns> a predicate returning true when a given record matches newRecord </returns>
-        protected virtual Predicate<string[]> CreateSimpleMatcher(string[] recordTemplate) {
-            int length = recordTemplate.Length;
-            return delegate(string[] record) {
-                if (length != record.Length)
-                    return false;
-
-                for (int i = 0; i < length; ++i) {
-                    if (record[i] != recordTemplate[i])   // NOTE: != is okay for strings
-                        return false;
-                }
-
-                return true;
-            };
-        }
-
-        /// <summary> factory method to create a predicate matcher for a single field </summary>
-        /// <param name="field"> the field index </param>
-        /// <param name="str"> the pattern to match against the field value </param>
-        /// <returns> true if matched, false otherwise </returns>
-        protected virtual Predicate<string[]> CreateSingleFieldRegexMatcher(int field, string str) {
-            Regex regex = new Regex(str);
-            return delegate(string[] record) {
-                return (field < record.Length && regex.IsMatch(record[field]));
-            };
-        }
-
-// Not Currently Used...
-
-        /// <summary> factory method to create a predicate matcher for a string[] recordTemplate containing regexes </summary>
-        /// <param name="newRecord"> the record to be matching against </param>
-        /// <returns> a predicate returning true when a given record matches newRecord </returns>
-        protected virtual Predicate<string[]> CreateRegexMatcher(string[] recordTemplate) {
-
-            // Close over regexes and length once and use them in the returned closure
-            int length = recordTemplate.Length;
-            Regex[] regexes = Array.ConvertAll<string, Regex>(recordTemplate, delegate(string str) {
-                if (str == null || str.Length == 0)
-                    return new Regex(".*");   // wildcard match anything
-                return new Regex(str);        // input was a regex
-            });
-
-            // Predicate that runs regexes against each string in the records
-            return delegate(string[] record) {
-                if (length != record.Length)
-                    return false;
-
-                for (int i = 0; i < length; ++i) {
-                    if (!regexes[i].IsMatch(record[i]))
-                        return false;
-                }
-
-                return true;
-            };
-
-        }
 
 // Driver
 
---
+++ trunk/7-Database/LocalDBTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _7_Database {
+
+    public class LocalDBTest {
+    }
+}
