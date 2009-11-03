1292fca85a6b6adb36da65ed495e5359fb391692
alexander.maskovyak <alexander.maskovyak@a9ce6336-9ca8-11de-8e8d-3741ea1c8a87>
2009-11-03 13:58:03 +0000

    Filled in some of RemoteDB's methods.  This has not yet been tested!  We need to look at the locking and access to prove that it is correct.  Can we use the Cell object to ensure this?

--- trunk/8-DatabaseWebService-RemoteDB/RemoteDB.cs
+++ trunk/8-DatabaseWebService-RemoteDB/RemoteDB.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 using _7_Database;
 
@@ -10,70 +11,151 @@ using DatabaseWebServiceSoapClient = _8_DatabaseWebService.DatabaseWebServiceSoa
 namespace _8_DatabaseWebService {
     
     /// <summary> serves as a proxy for calls on a database webservice. </summary>
-    /// <remarks> asynchronously communicates with the server, and fields only one request at a time. </remarks>
+    /// <remarks> asynchronously communicates with the server; fielding only one request at a time. </remarks>
     public class RemoteDB : IModel<string> {
 
 // fields
+
+        /// <summary> reference to the web service. </summary>
         protected DatabaseWebServiceSoapClient _service;
 
-// constructors
+        /// <summary> current result from an event. </summary>
+        protected object _asyncResult;
 
-        public RemoteDB()
-        {
+        /// <summary> controls access via locks. </summary>
+        protected object _monitor;
+
+// constructors
+        
+        /// <summary> default constructor. </summary>
+        public RemoteDB() {
+            // setup service and events
             _service = new DatabaseWebServiceSoapClient();
             _service.get_SizeCompleted += new EventHandler<get_SizeCompletedEventArgs>(Handle_getSizeCompleted);
             _service.SearchCompleted += new EventHandler<SearchCompletedEventArgs>(Handle_SearchCompleted);
             _service.EnterCompleted += new EventHandler<EnterCompletedEventArgs>(Handle_EnterCompleted);
             _service.RemoveCompleted += new EventHandler<RemoveCompletedEventArgs>(Handle_RemoveCompeted);
 
-            //_service.get_SizeCompleted += new EventHandler<_8_DatabaseWebService_RemoteDB._8_DatabaseWebService.get_SizeCompletedEventArgs
-           
+            // initialize fields
+            _asyncResult = null;
+            _monitor = new object();
         }
 
 // event handlers
 
         /// <summary> handler for size method call returns. </summary>
-        /// <param name="sender"></param>
-        /// <param name="args"></param>
+        /// <param name="sender"> object which triggered this handler. </param>
+        /// <param name="args"> values associated with this completed event. </param>
         protected void Handle_getSizeCompleted(object sender, get_SizeCompletedEventArgs args) {
-
+            lock (_monitor) {
+                // get result and notify everyone waiting for a value
+                _asyncResult = args.Result;
+                Monitor.PulseAll(_monitor);
+            }
         }
 
+        /// <summary> handler for search method call returns. </summary>
+        /// <param name="sender"> object which triggered this handler. </param>
+        /// <param name="args"> values associated with this completed event. </param>
         protected void Handle_SearchCompleted(object sender, SearchCompletedEventArgs args) {
-
+            lock (_monitor) {
+                // get result and notify everyone waiting for a value
+                _asyncResult = args.Result;
+                Monitor.PulseAll(_monitor);
+            }
         }
 
+        /// <summary> handler for enter method call returns. </summary>
+        /// <param name="sender"> object which triggered this handler. </param>
+        /// <param name="args"> values associated with this completed event. </param>
         protected void Handle_EnterCompleted(object sender, EnterCompletedEventArgs args) {
-
+            lock (_monitor) {
+                // get result and notify everyone waiting for a value
+                _asyncResult = args.Result;
+                Monitor.PulseAll(_monitor);
+            }
         }
 
+        /// <summary> handler for remove method call returns. </summary>
+        /// <param name="sender"> object which triggered this handler. </param>
+        /// <param name="args"> values associated with this completed event. </param>
         protected void Handle_RemoveCompeted(object sender, RemoveCompletedEventArgs args) {
-
+            lock (_monitor) {
+                // get result and notify everyone waiting for a value
+                _asyncResult = args.Result;
+                Monitor.PulseAll(_monitor);
+            }
         }
 
 // IModel interface
 
         /// <summary> entries in database. </summary>
         public int Size { 
-            get { throw new NotImplementedException(); }  
+            get {
+                lock (_monitor) {
+                    // do asynchronous call
+                    _service.get_SizeAsync();
+                    // wait for result
+                    Monitor.Wait(_monitor);
+                    // grab it
+                    int result = (int)_asyncResult;
+                    // give someone else access
+                    Monitor.Pulse(_monitor);
+
+                    return result;
+                }
+            }  
         }
         
         /// <summary> finds matching tuples. </summary>
         /// <returns> words to be shown in each field. </returns>
         public string[][] Search(string[] keys) {
-            throw new NotImplementedException(); 
+            lock (_monitor) {
+                // do asynchronous call
+                _service.SearchAsync(keys);
+                // wait for result
+                Monitor.Wait(_monitor);
+                // grab it
+                string[][] result = (string[][])_asyncResult;
+                // give someone else access
+                Monitor.Pulse(_monitor);
+
+                return result;
+            } 
         }
         
         /// <summary> adds (or replaces) a tuple. </summary>
         /// <returns> true if something was added (not replaced). </returns>
         public bool Enter(string[] tuple) {
-            throw new NotImplementedException(); 
+            lock (_monitor) {
+                // do asynchronous call
+                _service.EnterAsync(tuple);
+                // wait for result
+                Monitor.Wait(_monitor);
+                // grab it
+                bool result = (bool)_asyncResult;
+                // give someone else access
+                Monitor.Pulse(_monitor);
+
+                return result;
+            } 
         }
         
         /// <summary> removes tuples. </summary>
         /// <returns> returns true if something was removed. </returns>
         public bool Remove(string[] keys) {
-            throw new NotImplementedException(); 
+            lock (_monitor) {
+                // do asynchronous call
+                _service.RemoveAsync(keys);
+                // wait for result
+                Monitor.Wait(_monitor);
+                // grab it
+                int result = (int)_asyncResult;
+                // give someone else access
+                Monitor.Pulse(_monitor);
+
+                return result;
+            }
         }
 
         public static void Main(string[] args) {
