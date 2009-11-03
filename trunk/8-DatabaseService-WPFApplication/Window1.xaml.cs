a05b55a92111281e7d0773a499d08ae9e327cdbf
joepeck02 <joepeck02@a9ce6336-9ca8-11de-8e8d-3741ea1c8a87>
2009-11-03 19:47:05 +0000

    - Handle a Local and Remote reference - Get Connection information working for Remote (app.config) - Implemented Toggle Button

--- trunk/8-DatabaseService-WPFApplication/Window1.xaml.cs
+++ trunk/8-DatabaseService-WPFApplication/Window1.xaml.cs
@@ -22,17 +22,102 @@ namespace _8_DatabaseWebService
     /// </summary>
     public partial class Window1 : Window {
 
+// Constants
+
+        const string LocalDBMode = "Local";
+        const string RemoteDBMode = "Remote";
+
+// Fields
+
+        /// <summary> local database facade </summary>
+        protected IModel<string> _local;
+
+        /// <summary> remote database facade </summary>
+        protected IModel<string> _remote;
+
+        /// <summary> state of the UI </summary>
+        protected bool _isLocal;
+
 // Constructors
 
         /// <summary> default constructor </summary>
-        public Window1() : this( "Name", "Phone", "Room" ) { }
+        public Window1() : this( "Name", "Phone", "Room" ) {}
 
         /// <summary> constructor with names for tuple fields </summary>
         /// <param name="fields"> the tuple field names </param>
         public Window1(params string[] fields) {
             InitializeComponent();
             FieldsControl.AddFields(fields);
+
+            // NOTE: These are created lazily, as needed, in switchTo*()
+            _local = null;
+            _remote = null;
+
+            // Initialize with Local
+            switchToLocal();
         }
+
+// Factory Methods
+
+        /// <summary> factory method to create the "local" database </summary>
+        /// <returns> a new database </returns>
+        protected virtual IModel<string> createLocalDatabase() {
+            return new LocalDB();
+        }
+
+        /// <summary> factory method to create the "remote" database </summary>
+        /// <returns> a new database </returns>
+        protected virtual IModel<string> createRemoteDatabase() {
+            return new RemoteDB();
+        }
+
+// UI Methods
+
+        /// <summary> display the local database </summary>
+        protected virtual void switchToLocal() {
+            if (_isLocal)
+                return;
+
+            if (_local == null)
+                _local = createLocalDatabase();
+
+            _isLocal = true;
+            DBMode.Text = LocalDBMode; // needs GUI thread
+            switchToDatabase(_local);
+        }
+
+        /// <summary> display the remote database </summary>
+        protected virtual void switchToRemote() {
+            if (!_isLocal)
+                return;
+
+            if (_remote == null)
+                _remote = createRemoteDatabase();
+
+            _isLocal = false;
+            DBMode.Text = RemoteDBMode; // needs GUI thread
+            switchToDatabase(_remote);
+        }
+
+        /// <summary> generic method for displaying a database </summary>
+        /// <param name="database"> the database to display </param>
+        protected virtual void switchToDatabase(IModel<string> database) {
+            // TODO: Joe is implementing now
+        }
+
+
+// Button Listeners
+
+        /// <summary> clicked Toggle Button </summary>
+        private void Toggle_Click(object sender, RoutedEventArgs e) {
+            if (_isLocal) {
+                switchToRemote();
+            } else {
+                switchToLocal();
+            }
+        }
+
+
 
     }
 }
