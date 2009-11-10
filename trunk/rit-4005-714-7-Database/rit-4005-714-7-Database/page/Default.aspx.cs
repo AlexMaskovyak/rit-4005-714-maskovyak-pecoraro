58ac99ac1712087732579fb5504c9d9c1f528a64
joepeck02 <joepeck02@a9ce6336-9ca8-11de-8e8d-3741ea1c8a87>
2009-11-10 18:24:26 +0000

    - Access to "current" in the Switcher and placed "current" in the Session so maintain state across page loads

--- trunk/rit-4005-714-7-Database/rit-4005-714-7-Database/page/Default.aspx.cs
+++ trunk/rit-4005-714-7-Database/rit-4005-714-7-Database/page/Default.aspx.cs
@@ -10,17 +10,35 @@ using ATS.Database;
 namespace page
 {
     public partial class _Default : System.Web.UI.Page {
-       
 
+        protected void Page_Load(object sender, EventArgs e) {
+            Application.Lock();
+            try {
+                int current = (int)Session["current"];
+                ((Switcher)Application["switcher"]).Current = current;
+            } finally {
+                Application.UnLock();
+            }
+        }
+
+        protected void Page_Unload(object sender, EventArgs e) {
+            Application.Lock();
+            try {
+                Session["current"] = ((Switcher)Application["switcher"]).Current;
+            } finally {
+                Application.UnLock();
+            }
+        }
 
         protected void Page_Init(object sender, EventArgs e) {
             Application.Lock();
             try {
                 if (Session["db"] == null) Session["db"] = new LocalDB<string>();
+                if (Session["current"] == null) Session["current"] = 0;
                 if (Application["dbLocal"] == null) Application["dbLocal"] = new LocalDB<string>();
                 if (Application["dbRemote"] == null) Application["dbRemote"] = new RemoteDB();
 
-                new Switcher(
+                Application["switcher"] = new Switcher(
                     new Enable(isEnabled => {
                         Toggle.Enabled = Search.Enabled = Enter.Enabled = Remove.Enabled = isEnabled;
                     }),
@@ -31,11 +49,11 @@ namespace page
                         new Access(() => Phones.Text, s => { Phones.Text = s; }),
                         new Access(() => Rooms.Text, s => { Rooms.Text = s; })},
                     new SetClick[]{
-                        new SetClick((EventHandler eh) => { Toggle.Click += new EventHandler(eh); }),
-                        new SetClick((EventHandler eh) => { Search.Click += new EventHandler(eh); }),
-                        new SetClick((EventHandler eh) => { Enter.Click += new EventHandler(eh); }),
-                        new SetClick((EventHandler eh) => { Remove.Click += new EventHandler(eh); }),
-                        new SetClick((EventHandler eh) => { Size.Load += new EventHandler(eh); }) },
+                        new SetClick((EventHandler eh) => { Toggle.Click += eh; }),
+                        new SetClick((EventHandler eh) => { Search.Click += eh; }),
+                        new SetClick((EventHandler eh) => { Enter.Click += eh; }),
+                        new SetClick((EventHandler eh) => { Remove.Click += eh; }),
+                        new SetClick((EventHandler eh) => { Size.Load += eh; }) },
                     new WorkQueue(),
                         "session local DB", Session["db"], 
                         "application local DB", Application["dbLocal"], 
