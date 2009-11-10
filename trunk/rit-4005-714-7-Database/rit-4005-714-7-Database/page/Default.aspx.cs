c2e0be4f2738605c2b2d10eace2c7afb7cf21480
joepeck02 <joepeck02@a9ce6336-9ca8-11de-8e8d-3741ea1c8a87>
2009-11-10 22:35:02 +0000

    - Abstracted code better - Multiline textfields

--- trunk/rit-4005-714-7-Database/rit-4005-714-7-Database/page/Default.aspx.cs
+++ trunk/rit-4005-714-7-Database/rit-4005-714-7-Database/page/Default.aspx.cs
@@ -13,25 +13,37 @@ namespace page
     /// <summary> Default.aspx page handlers </summary>
     public partial class _Default : System.Web.UI.Page {
 
+// Constants
+
+        /// <summary> maintain current state in the session </summary>
+        const string CurrentKey = "current";
+
+        /// <summary> access to the Switcher in the session </summary>
+        const string SwitcherKey = "switcher";
+
+        /// <summary> session local database </summary>
+        const string SessionLocalKey = "sessionLocal";
+
+        /// <summary> application remote database </summary>
+        const string ApplicationRemoteKey = "applicationRemote";
+
+        /// <summary> application local database </summary>
+        const string ApplicationLocalKey = "applicationLocal";
+
+// Page Events
+
         /// <summary> set the state of the switcher for this session </summary>
         protected void Page_Load(object sender, EventArgs e) {
-            Application.Lock(); // substitute for Session.Lock
-            try {
-                int current = (int)Session["current"];
-                ((Switcher)Session["switcher"]).Current = current;
-            } finally {
-                Application.UnLock();
+            lock (Session) {
+                int current = (int)Session[CurrentKey];
+                ((Switcher)Session[SwitcherKey]).Current = current;
             }
-
         }
 
         /// <summary> store the state of the switcher for this session </summary>
         protected void Page_Unload(object sender, EventArgs e) {
-            Application.Lock(); // substitute for Session.Lock
-            try {
-                Session["current"] = ((Switcher)Session["switcher"]).Current;
-            } finally {
-                Application.UnLock();
+            lock (Session) {
+                Session[CurrentKey] = ((Switcher)Session[SwitcherKey]).Current;
             }
         }
 
@@ -39,31 +51,41 @@ namespace page
         protected void Page_Init(object sender, EventArgs e) {
             Application.Lock();
             try {
-                if (Session["db"] == null) Session["db"] = new LocalDB<string>();
-                if (Session["current"] == null) Session["current"] = 0;
-                if (Application["dbLocal"] == null) Application["dbLocal"] = new LocalDB<string>();
-                if (Application["dbRemote"] == null) Application["dbRemote"] = new RemoteDB();
 
-                Session["switcher"] = new Switcher(
-                    new Enable(isEnabled => {
-                        Toggle.Enabled = Search.Enabled = Enter.Enabled = Remove.Enabled = isEnabled;
-                    }),
-                    new IAccess[]{
-                        new Access(() => Current.Text, s => { Current.Text = s; }),
-                        new Access(() => Size.Text, s => { Size.Text = s; }),
-                        new Access(() => Names.Text, s => { Names.Text = s; }),
-                        new Access(() => Phones.Text, s => { Phones.Text = s; }),
-                        new Access(() => Rooms.Text, s => { Rooms.Text = s; })},
-                    new SetClick[]{
-                        new SetClick((EventHandler eh) => { Toggle.Click += eh; }),
-                        new SetClick((EventHandler eh) => { Search.Click += eh; }),
-                        new SetClick((EventHandler eh) => { Enter.Click += eh; }),
-                        new SetClick((EventHandler eh) => { Remove.Click += eh; }),
-                        new SetClick((EventHandler eh) => { Size.Load += eh; }) },
-                    new WorkQueue(),
-                        "session local DB", Session["db"], 
-                        "application local DB", Application["dbLocal"], 
-                        "application remote DB", Application["dbRemote"] );
+                if (Application[ApplicationRemoteKey] == null)
+                    Application[ApplicationRemoteKey] = new RemoteDB();
+
+                if (Application[ApplicationLocalKey] == null)
+                    Application[ApplicationLocalKey] = new LocalDB<string>();
+
+                lock (Session) {
+                    if (Session[SessionLocalKey] == null)
+                        Session[SessionLocalKey] = new LocalDB<string>();
+
+                    if (Session[CurrentKey] == null)
+                        Session[CurrentKey] = 0;
+
+                    Session[SwitcherKey] = new Switcher(
+                        new Enable(isEnabled => {
+                            Toggle.Enabled = Search.Enabled = Enter.Enabled = Remove.Enabled = isEnabled;
+                        }),
+                        new IAccess[]{
+                            new Access(() => Current.Text, s => { Current.Text = s; }),
+                            new Access(() => Size.Text, s => { Size.Text = s; }),
+                            new Access(() => Names.Text, s => { Names.Text = s; }),
+                            new Access(() => Phones.Text, s => { Phones.Text = s; }),
+                            new Access(() => Rooms.Text, s => { Rooms.Text = s; })},
+                        new SetClick[]{
+                            new SetClick((EventHandler eh) => { Toggle.Click += eh; }),
+                            new SetClick((EventHandler eh) => { Search.Click += eh; }),
+                            new SetClick((EventHandler eh) => { Enter.Click += eh; }),
+                            new SetClick((EventHandler eh) => { Remove.Click += eh; }),
+                            new SetClick((EventHandler eh) => { Size.Load += eh; }) },
+                        new WorkQueue(),
+                            "session local DB", Session[SessionLocalKey],
+                            "application local DB", Application[ApplicationLocalKey],
+                            "application remote DB", Application[ApplicationRemoteKey]);
+                }
                 
             } finally {
                 Application.UnLock();
