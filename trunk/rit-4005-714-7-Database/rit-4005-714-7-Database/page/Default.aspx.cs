d83e6639ce06b20cf85a78cdd2bae418ae9b1d12
alexander.maskovyak <alexander.maskovyak@a9ce6336-9ca8-11de-8e8d-3741ea1c8a87>
2009-11-10 15:58:46 +0000

    Working on fixing the page.  According to the slides this probably shouldn't be a asp.net application but a page.  We'll have to change that as well.

--- trunk/rit-4005-714-7-Database/rit-4005-714-7-Database/page/Default.aspx.cs
+++ trunk/rit-4005-714-7-Database/rit-4005-714-7-Database/page/Default.aspx.cs
@@ -12,24 +12,37 @@ namespace page
     public partial class _Default : System.Web.UI.Page {
        
 
-        protected void Page_Load(object sender, EventArgs e) {
-            new Switcher(
-                new Enable(isEnabled => {
-                    Toggle.Enabled = Search.Enabled = Enter.Enabled = Remove.Enabled = isEnabled; }),
-                new IAccess[]{
-                    new Access(() => Current.Text, s => { Current.Text = s; }),
-                    new Access(() => Size.Text, s => { Size.Text = s; }),
-                    new Access(() => Names.Text, s => { Names.Text = s; }),
-                    new Access(() => Phones.Text, s => { Phones.Text = s; }),
-                    new Access(() => Rooms.Text, s => { Rooms.Text = s; })},
-                new SetClick[]{
-                    new SetClick((EventHandler eh) => { Toggle.Click += new EventHandler(eh); }),
-                    new SetClick((EventHandler eh) => { Search.Click += new EventHandler(eh); }),
-                    new SetClick((EventHandler eh) => { Enter.Click += new EventHandler(eh); }),
-                    new SetClick((EventHandler eh) => { Remove.Click += new EventHandler(eh); }),
-                    new SetClick((EventHandler eh) => { Size.Load += new EventHandler(eh); }) },
-                new WorkQueue(),
-                    "local", new LocalDB<string>(), "remote", new RemoteDB());
+
+        protected void Page_Init(object sender, EventArgs e) {
+            Application.Lock();
+            try {
+                if (Session["db"] == null) Session["db"] = new LocalDB<string>();
+                if (Application["dbLocal"] == null) Application["dbLocal"] = new LocalDB<string>();
+                if (Application["dbRemote"] == null) Application["dbRemote"] = new RemoteDB();
+
+                new Switcher(
+                    new Enable(isEnabled => {
+                        Toggle.Enabled = Search.Enabled = Enter.Enabled = Remove.Enabled = isEnabled;
+                    }),
+                    new IAccess[]{
+                        new Access(() => Current.Text, s => { Current.Text = s; }),
+                        new Access(() => Size.Text, s => { Size.Text = s; }),
+                        new Access(() => Names.Text, s => { Names.Text = s; }),
+                        new Access(() => Phones.Text, s => { Phones.Text = s; }),
+                        new Access(() => Rooms.Text, s => { Rooms.Text = s; })},
+                    new SetClick[]{
+                        new SetClick((EventHandler eh) => { Toggle.Click += new EventHandler(eh); }),
+                        new SetClick((EventHandler eh) => { Search.Click += new EventHandler(eh); }),
+                        new SetClick((EventHandler eh) => { Enter.Click += new EventHandler(eh); }),
+                        new SetClick((EventHandler eh) => { Remove.Click += new EventHandler(eh); }),
+                        new SetClick((EventHandler eh) => { Size.Load += new EventHandler(eh); }) },
+                    new WorkQueue(),
+                        "session local DB", Session["db"], 
+                        "application local DB", Application["dbLocal"], 
+                        "application remote DB", Application["dbRemote"] );
+            } finally {
+                Application.UnLock();
+            }
         }
     }
 }
