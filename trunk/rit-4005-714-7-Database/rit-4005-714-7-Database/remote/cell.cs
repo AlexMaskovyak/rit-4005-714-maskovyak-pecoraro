a1761284b97e8287abcf79a642d7fb269e5c3f19
alexander.maskovyak <alexander.maskovyak@a9ce6336-9ca8-11de-8e8d-3741ea1c8a87>
2009-11-10 02:49:06 +0000

    Solution based upon Professor's solution.

---
+++ trunk/rit-4005-714-7-Database/rit-4005-714-7-Database/remote/cell.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace ATS {
+
+  /// <summary> producer/consumer pattern. </summary>
+  public class Cell<T> {
+
+    /// <summary> value to be passed. </summary>
+    private T content;
+
+    /// <summary> state of <c>content</c>. </summary>
+    private bool full;
+
+    /// <summary> thread-safe access to <c>content</c>. </summary>
+    public T Value {
+      get {
+        lock (this) {
+          while (!full) Monitor.Wait(this);
+          full = false;
+          Monitor.PulseAll(this);
+          return content;
+        }
+      }
+      set {
+        lock (this) {
+          while (full) Monitor.Wait(this);
+          full = true;
+          Monitor.PulseAll(this);
+          content = value;
+        }
+      }
+    }
+  }
+}
